527669f8efe2bcb466953c843ea96a9022e1d122
Bartek Wilczek <bartek.wilczek@toptal.com>
2019-07-01 21:31:08 +0200

    Parse lat/lon

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-# WIP
+# Ruby parser for SeeYou .cup files
 
-http://download.naviter.com/docs/CUP-file-format-description.pdf
+# THIS IS A VERY EARLY VERSION
 
+File format specs can be found [here](http://download.naviter.com/docs/CUP-file-format-description.pdf).
+
+Example:
+
+```
+require 'seeyoucup'
+
+waypoints = SeeYouCup::Parser.new("#{__dir__}/samples/basic.cup").parse
+wp = waypoints.first
+wp.name     # String
+wp.code     # String
+wp.country  # String
+wp.elev     # String
+wp.rwydir   # String
+wp.rwylen   # String
+wp.freq     # String
+wp.desc     # String
+wp.lat      # Float
+wp.lon      # Float
+```
+
+Limitations:
+
+* Only files containing ONLY `Waypoints` section are supported
+* Files that combine `Waypoints` and `Tasks` have not been tested
--- lib/see_you_cup/waypoint.rb
+++ lib/see_you_cup/waypoint.rb
@@ -1,5 +1,58 @@
 # frozen_string_literal: true
 
 module SeeYouCup
-  Waypoint = Struct.new(:name, :code, :country, :lat, :lon, :elev, :style, :rwydir, :rwylen, :freq, :desc, keyword_init: true) # rubocop:disable Metrics/LineLength
+  class Waypoint
+    STYLE_MAP = {
+      0 => 'Unknown',
+      1 => 'Waypoint',
+      2 => 'Airfield with grass surface runway',
+      3 => 'Outlanding',
+      4 => 'Gliding airfield',
+      5 => 'Airfield with solid surface runway',
+      6 => 'Mountain Pass',
+      7 => 'Mountain Top',
+      8 => 'Transmitter Mast',
+      9 => 'VOR',
+      10 => 'NDB',
+      11 => 'Cooling Tower',
+      12 => 'Dam',
+      13 => 'Tunnel',
+      14 => 'Bridge',
+      15 => 'Power Plant',
+      16 => 'Castle',
+      17 => 'Intersection'
+    }.freeze
+
+    attr_accessor :name, :code, :country, :elev, :rwydir, :rwylen, :freq, :desc
+    attr_reader :lat, :lon
+
+    def lon=(coord)
+      @lon = parse_coord(coord)
+    end
+
+    def lat=(coord)
+      @lat = parse_coord(coord)
+    end
+
+    def style=(style)
+      @style = style.to_i
+    end
+
+    def style
+      STYLE_MAP[@style]
+    end
+
+    private
+
+    def parse_coord(coord)
+      return coord if coord.is_a?(Float)
+
+      coord = coord.upcase.strip.gsub(/^0/, '')
+      sign = %w[N E].include?(coord[-1]) ? 1 : -1
+      coord.chop!
+      deg = coord[0..1].to_f
+      min = coord[2..7].to_f
+      sign * (deg + min / 60)
+    end
+  end
 end
